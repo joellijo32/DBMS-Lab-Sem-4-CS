3008994ee19f924659cd693e8cce7c463f257176
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-27 23:55:30 +0530

    5 b 7 query minimized

--- Main_Lab_Cycle/5_IMPLEMENTATION_OF_AGGREGATE_FUNCTIONS_ORDER_BY_GROUP_BY_&_HAVING_CLAUSE_IN_SQL/b_Order_By_Group_By_Having_clause_in_SQL./7_dept_list_condtn.sql
+++ Main_Lab_Cycle/5_IMPLEMENTATION_OF_AGGREGATE_FUNCTIONS_ORDER_BY_GROUP_BY_&_HAVING_CLAUSE_IN_SQL/b_Order_By_Group_By_Having_clause_in_SQL./7_dept_list_condtn.sql
@@ -1,6 +1,6 @@
-SELECT department.deptid, deptname, COUNT(employee.empid) AS number_of_employees, AVG(salary) AS average_salary 
-FROM department
-JOIN employee ON department.deptid = employee.deptid
-GROUP BY department.deptname, department.deptid
-HAVING COUNT(employee.empid) > 3
+SELECT d.deptid, deptname, COUNT(e.empid) AS number_of_employees, AVG(salary) AS average_salary 
+FROM department d
+JOIN employee e ON d.deptid = e.deptid
+GROUP BY d.deptname, d.deptid
+HAVING COUNT(e.empid) > 3
 ORDER BY average_salary ASC;
