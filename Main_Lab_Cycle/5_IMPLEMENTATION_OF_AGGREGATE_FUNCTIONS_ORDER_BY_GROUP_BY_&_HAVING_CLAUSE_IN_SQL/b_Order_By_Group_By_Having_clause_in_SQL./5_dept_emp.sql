38ec814d20551b25c1f5f9d53512e747d5e3e326
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-27 23:50:43 +0530

    5 b 5 query minimized

--- Main_Lab_Cycle/5_IMPLEMENTATION_OF_AGGREGATE_FUNCTIONS_ORDER_BY_GROUP_BY_&_HAVING_CLAUSE_IN_SQL/b_Order_By_Group_By_Having_clause_in_SQL./5_dept_emp.sql
+++ Main_Lab_Cycle/5_IMPLEMENTATION_OF_AGGREGATE_FUNCTIONS_ORDER_BY_GROUP_BY_&_HAVING_CLAUSE_IN_SQL/b_Order_By_Group_By_Having_clause_in_SQL./5_dept_emp.sql
@@ -1,5 +1,5 @@
-SELECT employee.deptid, department.deptname, COUNT(employee.empid) AS number_of_employees
-FROM department
-JOIN employee ON department.deptid = employee.deptid
-GROUP BY department.deptname, employee.deptid
-HAVING COUNT(employee.empid) > 5;
+SELECT e.deptid, d.deptname, COUNT(e.empid) AS number_of_employees
+FROM department d
+JOIN employee e ON d.deptid = e.deptid
+GROUP BY d.deptname, e.deptid
+HAVING COUNT(e.empid) > 5;
