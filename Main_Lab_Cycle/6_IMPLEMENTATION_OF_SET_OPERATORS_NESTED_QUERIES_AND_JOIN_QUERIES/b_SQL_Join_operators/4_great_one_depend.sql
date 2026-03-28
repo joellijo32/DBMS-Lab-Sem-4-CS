fa98f6a24e2c10f800532e335f03ac1d8271d6c4
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-28 14:57:33 +0530

    6 b 4 minimized

--- Main_Lab_Cycle/6_IMPLEMENTATION_OF_SET_OPERATORS_NESTED_QUERIES_AND_JOIN_QUERIES/b_SQL_Join_operators/4_great_one_depend.sql
+++ Main_Lab_Cycle/6_IMPLEMENTATION_OF_SET_OPERATORS_NESTED_QUERIES_AND_JOIN_QUERIES/b_SQL_Join_operators/4_great_one_depend.sql
@@ -1,5 +1,5 @@
-SELECT employee.empid, empname, COUNT(*) AS number_of_dependents
-FROM employee 
-JOIN dependent ON employee.empid = dependent.empid
-GROUP BY employee.empid
+SELECT e.empid, empname, COUNT(*) AS number_of_dependents
+FROM employee e
+JOIN dependent d ON e.empid = d.empid
+GROUP BY e.empid
 HAVING COUNT(*) > 1;
