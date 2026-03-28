96e006ad2b4fa9b38a1a760750bf69aed9b281b3
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-28 21:38:26 +0530

    10 a i modified

--- Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/a_Packages/i_proc_new_emp.sql
+++ Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/a_Packages/i_proc_new_emp.sql
@@ -1,17 +1,9 @@
-CREATE OR REPLACE PROCEDURE company_pkg.add_employee(
- p_EmpID INT,
- p_Ename VARCHAR,
- p_DOB DATE,
- p_City VARCHAR,
- p_DOJ DATE,
- p_Salary NUMERIC,
- p_DeptID INT
-)
+CREATE OR REPLACE PROCEDURE company_pkg.add_employee(p_EmpID INT, p_Ename VARCHAR, p_DOB DATE, p_City VARCHAR, p_DOJ DATE, p_Salary NUMERIC, p_DeptID INT)
 LANGUAGE plpgsql
 AS $$
 BEGIN
  	INSERT INTO EMPLOYEE VALUES (p_EmpID, p_Ename, p_DOB, p_City, p_DOJ,
 			             p_Salary, p_DeptID);
-		 RAISE NOTICE 'Employee inserted successfully';
+	RAISE NOTICE 'Employee inserted successfully';
 END;
 $$;
