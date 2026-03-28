62fc7fb707f30d1ae60ce2edbec0f833566fddba
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-28 21:28:08 +0530

    10 b i complete

---
+++ Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/b_Cursors/i_emp_dept.sql
@@ -0,0 +1,14 @@
+DO $$
+DECLARE emp_cursor CURSOR FOR
+	SELECT EmpID, Empname, Salary FROM EMPLOYEE WHERE DeptID = 101;
+	emp_record RECORD;
+BEGIN
+	OPEN emp_cursor;
+	LOOP
+		FETCH emp_cursor INTO emp_record;
+		EXIT WHEN NOT FOUND;
+		RAISE NOTICE 'EmpID: %, Name: %, Salary: %',emp_record.EmpID, emp_record.Empname, emp_record.Salary;
+	END LOOP;
+	CLOSE emp_cursor;
+END;
+$$;
