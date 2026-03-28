6462fef9661e3c4870d1be9bc6aa56391e10c7cc
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-28 21:07:16 +0530

    10 a iv complete

---
+++ Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/a_Packages/iv_emp_dept_detail.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE PROCEDURE company_pkg.display_employee_details()
+LANGUAGE plpgsql
+AS $$
+DECLARE rec RECORD;
+BEGIN
+    FOR rec IN
+        SELECT e.EmpID, e.Empname, d.DeptName, e.Salary
+        FROM EMPLOYEE e
+        JOIN DEPARTMENT d
+        ON e.DeptID = d.DeptID
+    LOOP
+        RAISE NOTICE 'EmpID: %, Name: %, Department: %, Salary: %',
+        rec.EmpID, rec.Empname, rec.DeptName, rec.Salary;
+    END LOOP;
+END;
+$$;
+
+
+CALL company_pkg.display_employee_details();
