1a05f8ce4dcaeb061ea17f9d247be27761e0b5c0
Joel Lijo Mathew <joellijo4@gmail.com>
2026-03-30 15:08:27 +0530

    Fix case sensitivity in SQL queries

--- Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/a_Packages/iii_dept_op.sql
+++ Main_Lab_Cycle/10_PACKAGES_AND_CURSORS/a_Packages/iii_dept_op.sql
@@ -3,14 +3,14 @@ LANGUAGE plpgsql
 AS $$
 DECLARE v_old_dept INT;
 BEGIN
-	SELECT DeptID INTO v_old_dept FROM EMPLOYEE WHERE EmpID = p_empid;
+	SELECT DeptID INTO v_old_dept FROM employee WHERE EmpID = p_empid;
 	IF NOT FOUND THEN
 		RAISE EXCEPTION 'Employee does not exist';
 	END IF;
 
-	UPDATE EMPLOYEE SET DeptID = p_new_dept WHERE EmpID = p_empid;
-	UPDATE DEPARTMENT SET Fund = Fund * 0.95 WHERE DeptID = v_old_dept;
-	UPDATE DEPARTMENT SET Fund = Fund * 1.05 WHERE DeptID = p_new_dept;
+	UPDATE employee SET DeptID = p_new_dept WHERE EmpID = p_empid;
+	UPDATE department SET Fund = Fund * 0.95 WHERE DeptID = v_old_dept;
+	UPDATE department SET Fund = Fund * 1.05 WHERE DeptID = p_new_dept;
 	RAISE NOTICE 'Employee transferred successfully';
 END;
 $$;
